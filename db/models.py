505caf2a1a562b8f3ff9fc506581b47a93025dfe
Mohammad Baharlou <75358012+iimohammad@users.noreply.github.com>
2024-01-30 13:34:28 +0330

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,1 +1,40 @@
 
+
+class person_model:
+    def __init__(self,username) -> None:
+        username = self.username
+        subscription = None
+
+class user_BankAccount_model:
+    def __init__(self) -> None:
+        pass
+
+
+
+class user_Wallet_model:
+    def __init__(self) -> None:
+        pass
+
+
+class movie_showtimes_model:
+    def __init__(self) -> None:
+        pass
+
+
+class chairs_showtimes_model:
+    def __init__(self) -> None:
+        pass
+
+
+class admin_model(person_model):
+    def __init__(self, username) -> None:
+        super().__init__(username)
+
+
+class users_model(person_model):
+    def __init__(self, username) -> None:
+        super().__init__(username)
+
+class comments_model:
+    def __init__(self) -> None:
+        pass
