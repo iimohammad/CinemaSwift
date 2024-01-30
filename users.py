c07b6ed640c9e1b3fd3e5abc1608dd8048dbeaa9
masih1999-12 <msh77742@gmail.com>
2024-01-30 23:39:49 +0330

    add some static ethod for users class

--- users.py
+++ users.py
@@ -1,10 +1,31 @@
 from db import models
+import re
 
 
-
-class users:
-    def __init__():
+class Users:
+    def __init__(self):
+        self.users = dict()
+        self.admins = dict()
+        
+    @staticmethod
+    def hashPassword(password:str):
+        return hash(password)
+    
+    @staticmethod
+    def updateUser(user:models.users_model):
         pass
     
-
-
+    @staticmethod
+    def emailValidator(email:str):
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        match = re.match(pattern, email)
+        if match:
+            return True
+        return False
+    @staticmethod
+    def phoneValidator(phone:str):
+        pattern = r'^09\d{9}$'
+        match = re.match(pattern, phone)
+        if match:
+            return True
+        return False
