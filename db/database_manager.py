dc0011e58b7ed3b67ddd95c9bd6ceb49042fa531
Mohammad Baharlou <mohammadbaharloo96@gmail.com>
2024-02-02 03:23:57 +0330

    database updated

--- db/database_manager.py
+++ db/database_manager.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from settings import local_settings
-
+import models
+import inspect
 
 class DatabaseManager:
     def __init__(self, host, user, password, database):
@@ -9,6 +10,12 @@ class DatabaseManager:
         self.password = password
         self.database = database
         self.connection = None
+
+        self.dict = {
+            'str': 'VARCHAR(255) NOT NULL',
+            'int': 'Integer',
+            'datetime':'DATE'
+        }
 
     def connect(self):
         try:
@@ -84,6 +91,11 @@ class DatabaseManager:
         except mysql.connector.Error as error:
             print("Error while selecting data:", error)
 
+    def create_columns(self,model_name):
+        init_signature = inspect.signature(model_name.__init__)
+        init_params = {param.name: param.annotation for param in init_signature.parameters.values() if param.name != 'self'}
+        person_model_columns = [(name, self.dict.get(param_type.__name__, 'UNKNOWN')) for name, param_type in init_params.items()]
+        return person_model_columns
 
 if __name__ == "__main__":
     db_manager = DatabaseManager(
@@ -96,117 +108,20 @@ if __name__ == "__main__":
     # Connect to MySQL
     db_manager.connect()
 
-    # Create desired tables
-    person_model_columns = [
-        ("username", "VARCHAR(255) NOT NULL"),
-        ("email", "VARCHAR(255) NOT NULL"),
-        ("birthday", "DATE"),
-        ("phone", "VARCHAR(255) NOT NULL"),
-    ]
-
-    # # Example usage: insert data into the table
-    # person_values = ["JohnDoe", "john.doe@example.com", "1990-01-01", "+123456789"]
-    # db_manager.insert_into_table("person_model", ["username", "email", "birthday", "phone"], person_values)
-
-    # # Example usage: select data from the table
-    # db_manager.select_column_from_table("person_model", "username")
-
-    bank_accounts_columns = [
-        ("id", "SERIAL"),
-        ("user_id", "INTEGER"),
-        ("balance", "DECIMAL(10, 2)"),
-        ("name", "VARCHAR(255)"),
-        ("cvv2", "VARCHAR(10)"),
-        ("password", "VARCHAR(255)"),
-    ]
-
-    wallets_columns = [
-        ("id", "SERIAL"),
-        ("balance", "DECIMAL(10, 2)"),
-        ("name", "VARCHAR(255)"),
-        ("user_id", "INTEGER"),
-    ]
-
-    seats_showtimes_columns = [
-        ("id", "SERIAL"),
-        ("sans_id", "INTEGER"),
-        ("status", "VARCHAR(255)"),
-    ]
-
-    sans_model_columns = [
-        ("id", "SERIAL"),
-        ("screen_id", "INTEGER"),
-        ("start_time", "TIMESTAMP"),
-        ("capacity", "INTEGER"),
-    ]
-
-    admin_model_columns = [
-        ("username", "VARCHAR(255) NOT NULL"),
-        ("email", "VARCHAR(255) NOT NULL"),
-        ("birthday", "DATE"),
-        ("phone", "VARCHAR(255) NOT NULL"),
-    ]
-
-    users_model_columns = [
-        ("username", "VARCHAR(255) NOT NULL"),
-        ("email", "VARCHAR(255) NOT NULL"),
-        ("birthday", "DATE"),
-        ("phone", "VARCHAR(255) NOT NULL"),
-        ("subscription_id", "INTEGER"),
-    ]
-
-    subscription_model_columns = [
-        ("id", "SERIAL"),
-        ("name", "VARCHAR(255)"),
-        ("descount_number", "INTEGER"),
-        ("discount_value", "DECIMAL(10, 2)"),
-        ("drink_number", "INTEGER"),
-    ]
-
-    comments_model_columns = [
-        ("id", "SERIAL"),
-        ("film_id", "INTEGER"),
-        ("user_id", "INTEGER"),
-        ("text", "TEXT"),
-        ("date", "TIMESTAMP"),
-        ("parent_comments_id", "INTEGER"),
-    ]
-
-    free_drinks_model_columns = [
-        ("id", "SERIAL"),
-        ("datetime", "TIMESTAMP"),
-    ]
-
-    screens_mode_columns = [
-        ("id", "SERIAL"),
-        ("film_id", "INTEGER"),
-        ("number_of_screens", "INTEGER"),
-    ]
-
-    films_model_columns = [
-        ("id", "SERIAL"),
-        ("name", "VARCHAR(255)"),
-        ("age_rating", "INTEGER"),
-        ("duration", "INTEGER"),
-        ("rate", "DECIMAL(3, 2)"),
-    ]
-
-    db_manager.create_table("person_model", person_model_columns)
-    db_manager.create_table("person_model", person_model_columns)
-    db_manager.create_table("bank_accounts_models", bank_accounts_columns)
-    db_manager.create_table("wallets_model", wallets_columns)
-    db_manager.create_table("seats_showtimes_model", seats_showtimes_columns)
-    db_manager.create_table("sans_model", sans_model_columns)
-    db_manager.create_table("admin_model", admin_model_columns)
-    db_manager.create_table("users_model", users_model_columns)
-    db_manager.create_table("subscription_model", subscription_model_columns)
-    db_manager.create_table("comments_model", comments_model_columns)
-    db_manager.create_table("free_drinks_model", free_drinks_model_columns)
-    db_manager.create_table("screens_mode", screens_mode_columns)
-    db_manager.create_table("films_model", films_model_columns)
+    db_manager.create_table("person_model", db_manager.create_columns(models.person_model))
+    db_manager.create_table("bank_accounts_models", db_manager.create_columns(models.bank_accounts_models))
+    db_manager.create_table("wallets_model", db_manager.create_columns(models.wallets_model))
+    db_manager.create_table("seats_showtimes_model", db_manager.create_columns(models.seats_showtimes_model))
+    db_manager.create_table("sans_model", db_manager.create_columns(models.sans_model))
+    db_manager.create_table("admin_model", db_manager.create_columns(models.admin_model))
+    db_manager.create_table("users_model", db_manager.create_columns(models.users_model))
+    db_manager.create_table("subscription_model", db_manager.create_columns(models.subscription_model))
+    db_manager.create_table("comments_model", db_manager.create_columns(models.comments_model))
+    db_manager.create_table("free_drinks_model", db_manager.create_columns(models.free_drinks_model))
+    db_manager.create_table("screens_mode", db_manager.create_columns(models.screens_mode))
+    db_manager.create_table("films_model", db_manager.create_columns(models.films_model))
 
     # Disconnect from MySQL
     db_manager.disconnect()
 
 
-# Call create_table for each class
